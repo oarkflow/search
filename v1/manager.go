4d4891b379ed626d852471d383440e5d0d1e7cc0
sujit <s.baniya.np@gmail.com>
2025-04-28 17:30:56 +0545

    feat: Offloading least used index

--- v1/manager.go
+++ v1/manager.go
@@ -1,15 +1,19 @@
 package v1
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"reflect"
+	"slices"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/oarkflow/filters"
 	"github.com/oarkflow/json"
 )
 
@@ -93,6 +97,69 @@ type NewIndexRequest struct {
 	ID string `json:"id"`
 }
 
+type Request struct {
+	Filters   []*filters.Filter `json:"filters"`
+	Query     string            `json:"q" query:"q"`
+	Condition string            `json:"condition" query:"condition"`
+	Match     string            `json:"m" query:"m"`
+	Fields    []string          `json:"f" query:"f"`
+	Offset    int               `json:"o" query:"o"`
+	Size      int               `json:"s" query:"s"`
+	SortField string            `json:"sort_field" query:"sort_field"`
+	SortOrder string            `json:"sort_order" query:"sort_order"`
+	Page      int               `json:"p" query:"p"`
+	Reverse   bool              `json:"reverse" query:"reverse"`
+}
+
+var builtInFields = []string{"q", "m", "l", "f", "t", "o", "s", "e", "p", "condition", "sort_field", "sort_order"}
+
+func prepareQuery(r *http.Request) (Request, error) {
+	var query Request
+	extraMap := make(map[string]any)
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return query, err
+	}
+	r.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	if bodyBytes != nil && len(bodyBytes) > 0 {
+		err = json.Unmarshal(bodyBytes, &query)
+		if err != nil {
+			return query, fmt.Errorf("error unmarshalling query: %v", err)
+		}
+		err = json.Unmarshal(bodyBytes, &extraMap)
+		if err != nil {
+			return query, fmt.Errorf("error unmarshalling extra: %v", err)
+		}
+	}
+	var extra []*filters.Filter
+	for k, v := range extraMap {
+		if slices.Contains(builtInFields, k) {
+			continue
+		}
+		vt := reflect.TypeOf(v).Kind()
+		operator := filters.Equal
+		if vt == reflect.Slice {
+			operator = filters.In
+		}
+		extra = append(extra, filters.NewFilter(k, operator, v))
+	}
+	if len(extra) == 0 {
+		rawQuery := r.URL.RawQuery
+		extra, err = filters.ParseQuery(rawQuery, builtInFields...)
+		if err != nil {
+			return query, err
+		}
+	}
+	if extra != nil && query.Filters == nil {
+		query.Filters = extra
+	}
+	query.Match = "AND"
+	if strings.ToLower(query.Match) == "any" {
+		query.Match = "OR"
+	}
+	return query, nil
+}
+
 func (m *Manager) StartHTTP(addr string) {
 	http.HandleFunc("/index/add", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
@@ -166,7 +233,6 @@ func (m *Manager) StartHTTP(addr string) {
 		}
 		w.Write([]byte("Index built successfully"))
 	})
-
 	http.HandleFunc("/{index}/search", func(w http.ResponseWriter, r *http.Request) {
 		indexName := r.PathValue("index")
 		if strings.TrimSpace(indexName) == "" {
